79436740271202d011c5998c90d0e79b7e1dc237
egodasa <egodasa@gmail.com>
2020-12-08 00:31:53 +0700

    Perbaikan kode di main activity

--- app/src/main/java/com/example/pakakeh/MainActivity.kt
+++ app/src/main/java/com/example/pakakeh/MainActivity.kt
@@ -9,62 +9,57 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setEvent()
+    }
 
-        // setiap control pada view harus dimasukan ke variabel
-        val Bhitung: Button = findViewById(R.id.Bhitung)
-        val TtinggiBadan: EditText = findViewById(R.id.TtinggiBadan)
-        val TberatBadan: EditText = findViewById(R.id.TberatBadan)
-        val Lhasil: TextView = findViewById(R.id.Lhasil)
-        val Bselanjutnya: TextView = findViewById(R.id.Bselanjutnya)
+    private fun setEvent() {
+        this.Bselanjutnya.setOnClickListener(BukaDetailUser)
+        this.Bhitung.setOnClickListener(HitungBmi)
+    }
 
-        // event on click pada button
-        Bhitung.setOnClickListener {
-            /* ambil instance textview */
-            var teks: String = ""
-            var warnaTeks: Int = Color.BLACK
-            try {
-                val bmi: Double = this.hitungBmi(
-                    TberatBadan.text.toString().toDouble(),
-                    TtinggiBadan.text.toString().toDouble()
-                )
-                teks += bmi.toString()
-                if (bmi < 18.5) {
-                    teks += ", Pakakeh Anda kurat berat"
-                    warnaTeks = Color.GREEN
-                } else if (bmi >= 18.5 && bmi <= 24.9) {
-                    teks += ", Pakakeh normal"
-                    warnaTeks = Color.BLUE
-                } else if (bmi >= 25.0 && bmi <= 29.9) {
-                    teks += ", Kelebihan berat pakakeh"
-                    warnaTeks = Color.MAGENTA
-                } else {
-                    teks += ", Pakakeh obesitas"
-                    warnaTeks = Color.RED
-                }
-            } catch (e: Exception) {
-                teks = "Gagal menghitung BMI. Silahkan ulangi lagi!"
-                warnaTeks = Color.BLACK
-            } finally {
-                Lhasil.setText(teks)
-                Lhasil.setTextColor(warnaTeks)
+    private val HitungBmi = View.OnClickListener {
+        var teks = ""
+        var warnaTeks: Int = Color.BLACK
+        try {
+            val bmi: Double = this.hitungBmi(
+                TberatBadan.text.toString().toDouble(),
+                TtinggiBadan.text.toString().toDouble()
+            )
+            teks += bmi.toString()
+            if (bmi < 18.5) {
+                teks += ", Pakakeh Anda kurat berat"
+                warnaTeks = Color.GREEN
+            } else if (bmi >= 18.5 && bmi <= 24.9) {
+                teks += ", Pakakeh normal"
+                warnaTeks = Color.BLUE
+            } else if (bmi >= 25.0 && bmi <= 29.9) {
+                teks += ", Kelebihan berat pakakeh"
+                warnaTeks = Color.MAGENTA
+            } else {
+                teks += ", Pakakeh obesitas"
+                warnaTeks = Color.RED
             }
-        }
-
-        // contoh event click
-        // kurung kurawal dibawah adalah lambda function 
-        Bselanjutnya.setOnClickListener{
-            // pindah activity
-            startActivity(Intent(this@MainActivity, DetailUser::class.java))
+        } catch (e: Exception) {
+            teks = "Gagal menghitung BMI. Silahkan ulangi lagi!"
+            warnaTeks = Color.BLACK
+        } finally {
+            Lhasil.setText(teks)
+            Lhasil.setTextColor(warnaTeks)
         }
     }
 
-    fun hitungBmi(berat: Double, tinggi: Double): Double {
+    private val BukaDetailUser = View.OnClickListener {
+        startActivity(Intent(this@MainActivity, DetailUser::class.java))
+    }
+
+    private fun hitungBmi(berat: Double, tinggi: Double): Double {
         return berat / (tinggi * tinggi)
     }
 }
